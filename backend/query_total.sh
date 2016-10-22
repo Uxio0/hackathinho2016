1a2e2f8358c0ad8121607023a9ffc27b7aab3e2b
Alejo Pacin Jul <alejopj@gmail.com>
2016-10-22 20:00:04 +0200

    Add new curl queries

---
+++ backend/query_total.sh
@@ -0,0 +1,6 @@
+curl "https://api.esios.ree.es/indicators/10043?start_date=2016-09-29T00:00:00+00:00&end_date=2016-09-30T00:00:00+00:00" -X GET \
+            -H "Accept: application/json; application/vnd.esios-api-v1+json" \
+            -H "Content-Type: application/json" \
+            -H "Host: api.esios.ree.es" \
+            -H "Authorization: Token token=\"5c7f9ca844f598ab7b86bffcad08803f78e9fc5bf3036eef33b5888877a04e38\"" \
+            -H "Cookie: "
